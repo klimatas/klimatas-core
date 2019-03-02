61d38f4c9ec599604248e7dadb2fecf2f3f4ae05
Mitchel van Amstel <mitchel@nldeploy.nl>
2019-03-02 12:40:38 +0100

    TX count

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -61,7 +61,7 @@ static Checkpoints::MapCheckpoints mapCheckpoints =
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
     1551519225, // * UNIX timestamp of last checkpoint block
-    0,    // * total number of transactions between genesis and last checkpoint
+    105326,    // * total number of transactions between genesis and last checkpoint
                 //   (the tx=... number in the SetBestChain debug.log lines)
     2000        // * estimated number of transactions per day after checkpoint
 };
