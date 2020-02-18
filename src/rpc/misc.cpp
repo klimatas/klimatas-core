d46f1d4231402f84ab5a42848e31bd0eadde5c5a
Mitchel van Amstel <m.vanamstel@dutchcrypto.works>
2020-02-18 20:41:06 +0100

    Blockchain synced true

--- src/rpc/misc.cpp
+++ src/rpc/misc.cpp
@@ -177,9 +177,9 @@ UniValue mnsync(const UniValue& params, bool fHelp)
     if (params.size() == 1)
         strMode = params[0].get_str();
 
-    if (fHelp || params.size() != 1 || (strMode != "status" && strMode != "reset")) {
+    if (fHelp || params.size() != 1 || (strMode != "status" && strMode != "reset" && strMode != "next")) {
         throw std::runtime_error(
-            "mnsync \"status|reset\"\n"
+            "mnsync \"status|next|reset\"\n"
             "\nReturns the sync status or resets sync.\n"
 
             "\nArguments:\n"
@@ -210,6 +210,12 @@ UniValue mnsync(const UniValue& params, bool fHelp)
 
             "\nExamples:\n" +
             HelpExampleCli("mnsync", "\"status\"") + HelpExampleRpc("mnsync", "\"status\""));
+    }
+
+    if(strMode == "next")
+    {
+        masternodeSync.GetNextAsset();
+        return "sync updated";
     }
 
     if (strMode == "status") {
